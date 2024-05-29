b6a5325ad86416e873b1c6693d9439ac1acb24c2
Julian Lima <julianlimalbs@gmail.com>
2024-05-29 13:25:43 -0300

    Password checker

---
+++ PasswordChecker/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace PasswordChecker;
+
+class Program
+{
+  /**
+    * Ask user to enter password, and store it
+    * Ask user to enter password again, and store it
+    * Check if they both contain something
+      * If so, check if they are the same
+        * If they are, print "Passwords match"
+        * If they are not, print "Passwords do not match"
+      * If they are empty, print "Please enter a password
+  */
+  static void Main(string[] args)
+  {
+    string? password = string.Empty;
+    string? passwordConfirm = string.Empty;
+
+    while (string.IsNullOrEmpty(password))
+    {
+      Console.Write("Enter your password: ");
+      password = Console.ReadLine();
+    }
+
+    while (string.IsNullOrEmpty(passwordConfirm))
+    {
+      Console.Write("Confirm your password: ");
+      passwordConfirm = Console.ReadLine();
+    }
+
+    if (password.Equals(passwordConfirm)) Console.WriteLine("Passwords match");
+    else Console.WriteLine("Passwords do not match");
+  }
+}
