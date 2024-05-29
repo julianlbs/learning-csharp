d3452591cc52e7f7cdc2f01c54d51da062cd836f
Julian Lima <julianlimalbs@gmail.com>
2024-05-29 08:58:37 -0300

    Fizz Buzz

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,22 @@
+﻿/**
+  Create a for loop from 1 to X (15)
+  3 and 5 = FizzBuzz
+  3 = Fizz
+  5 = Buzz
+  else = number
+ */
+
+Console.Write("Enter a number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+bool threeDiv = false;
+bool fiveDiv = false;
+
+for (int i = 0; i <= number; i++)
+{
+  threeDiv = i % 3 == 0;
+  fiveDiv = i % 5 == 0;
+  if (threeDiv && fiveDiv) Console.WriteLine("FizzBuzz");
+  else if (threeDiv) Console.WriteLine("Fizz");
+  else if (fiveDiv) Console.WriteLine("Buzz");
+  else Console.WriteLine(i);
+}
