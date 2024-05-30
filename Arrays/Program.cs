2e6e5578f7f60402054aaac8c59ea9f38e0477f7
Julian Lima <julianlimalbs@gmail.com>
2024-05-30 08:59:03 -0300

    Add Arrays

---
+++ Arrays/Program.cs
@@ -0,0 +1,57 @@
+﻿/*
+  * Arrays have a fixed size.
+  * Lists can grow and shrink.
+  * Lists are more flexible than arrays.
+  * Lists are more flexible than arrays.
+*/
+
+int[] numbers = new int[3];
+
+for (int i = 0; i < numbers.Length; i++)
+{
+  string? userInput = string.Empty;
+  bool isInt = false;
+  while (string.IsNullOrEmpty(userInput) && !isInt)
+  {
+    Console.Write($"Enter a number for index {i} : ");
+    userInput = Console.ReadLine();
+    if (int.TryParse(userInput, out int result))
+    {
+      isInt = true;
+      numbers[i] = result;
+    }
+    else
+    {
+      userInput = null;
+      Console.WriteLine("Invalid input");
+    }
+  }
+}
+
+
+int[] numbersArray = [5, 8, 3, 9, 1, 6, 7, 2, 4];
+Array.Sort(numbersArray);
+foreach (int num in numbersArray) Console.Write($"{num} | ");
+Console.WriteLine();
+
+Array.Reverse(numbersArray);
+foreach (int num in numbersArray) Console.Write($"{num} | ");
+Console.WriteLine();
+Array.Reverse(numbersArray);
+
+Console.Write("Enter a number to search: ");
+int search = Convert.ToInt32(Console.ReadLine());
+int position = Array.IndexOf(numbersArray, search);
+
+if (position != -1) Console.WriteLine($"Number in position {position}: {numbersArray[position]}");
+else Console.WriteLine($"Number {search} was not found");
+
+
+Array.Clear(numbersArray);
+
+// #################### Lists
+
+List<int> listNumbers = [];
+listNumbers.Add(5);
+listNumbers.Add(8);
+listNumbers.Add(3);
