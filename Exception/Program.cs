57b3749ba4957cd575636c9ce3e4ac9c67538c7e
Julian Lima <julianlimalbs@gmail.com>
2024-06-03 15:46:32 -0300

    Exception

---
+++ Exception/Program.cs
@@ -0,0 +1,24 @@
+﻿bool looping = true;
+
+while (looping)
+{
+  try
+  {
+    Console.Write("Enter a number: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine(num);
+    looping = false;
+  }
+  catch (OverflowException e)
+  {
+    Console.WriteLine($"{e.Message}\nPlease enter a number under 2 billion!");
+  }
+  catch (FormatException e)
+  {
+    Console.WriteLine($"{e.Message}\nPlease only enter numbers!");
+  }
+  catch (Exception e)
+  {
+    Console.WriteLine($"Error: {e}");
+  }
+}
