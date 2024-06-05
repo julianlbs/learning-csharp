5ac955f94acb3830bc6a6311302f0ff683997130
Julian Lima <julianlimalbs@gmail.com>
2024-06-05 07:21:43 -0300

    Custom TryParser

---
+++ CustomTryParse/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace CustomTryParse;
+
+class Program
+{
+  /*
+    * Create a custom try parse function
+    * Find the real function and copy return type/params
+    * Read the tooltip it gives you, to give you ideas on what to do
+  */
+  static void Main(string[] args)
+  {
+    bool success = false;
+    while (!success)
+    {
+      Console.Write("Enter a number: ");
+      string? input = Console.ReadLine();
+      if (!string.IsNullOrEmpty(input) && TryParse(input, out int num))
+      {
+        success = true;
+        Console.WriteLine(num);
+      }
+
+    }
+  }
+
+  static bool TryParse(string input, out int result)
+  {
+    result = -1;
+    try
+    {
+      result = Convert.ToInt32(input);
+      return true;
+    }
+    catch (Exception)
+    {
+      return false;
+    }
+  }
+}
