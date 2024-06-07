f966bb5b472ee22fbbfef822b318212d0826983a
Julian Lima <julianlimalbs@gmail.com>
2024-06-07 08:17:22 -0300

    Add Platform repository

--- Microservices/PlatformService/Program.cs
+++ Microservices/PlatformService/Program.cs
@@ -5,9 +5,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("InMemoryDb"));
+builder.Services.AddScoped<IPlatformRepo, PlatformRepo>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("InMemoryDb"));
 
 var app = builder.Build();
 
