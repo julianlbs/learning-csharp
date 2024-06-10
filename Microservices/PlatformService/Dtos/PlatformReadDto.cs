4207a6c779641cf71b8f6cc976b4bfb0de0c40c7
Julian Lima <julianlimalbs@gmail.com>
2024-06-10 07:54:04 -0300

    Create Dtos and Platform Profile

---
+++ Microservices/PlatformService/Dtos/PlatformReadDto.cs
@@ -0,0 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace PlatformService.Dtos;
+
+public class PlatformReadDto
+{
+  public int Id { get; set; }
+  public required string Name { get; set; }
+  public required string Publisher { get; set; }
+  public required string Cost { get; set; }
+}
